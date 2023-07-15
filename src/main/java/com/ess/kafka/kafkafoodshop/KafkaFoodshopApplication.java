86b68115f81999e9a519d050899e4c36040ab985
ESS Systemes & Services <lyrbat@gmail.com>
2023-07-15 11:05:39 +0200

    First commit.

---
+++ src/main/java/com/ess/kafka/kafkafoodshop/KafkaFoodshopApplication.java
@@ -0,0 +1,13 @@
+package com.ess.kafka.kafkafoodshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFoodshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaFoodshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ess/kafka/kafkafoodshop/KafkaFoodshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ess.kafka.kafkafoodshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaFoodshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
